0e7fa80d0975a8208ceda1eb4e9fe67b2da22518
andreivarga36 <andreivarga36@yahoo.com>
2024-04-30 11:10:35 +0300

    Default scripts were removed. HotelBooking class was implemented. New nuget packages were added

---
+++ HotelBookingApi/Models/HotelBooking.cs
@@ -0,0 +1,11 @@
+﻿namespace HotelBookingApi.Models
+{
+    public class HotelBooking
+    {
+        public int Id { get; set; }
+
+        public int RoomNumber { get; set; }
+
+        public string? ClientName { get; set; }
+    }
+}
