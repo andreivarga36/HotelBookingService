e9f6d2283c229c6f80d4296fda2b1c97434f2d82
andreivarga36 <andreivarga36@yahoo.com>
2024-05-01 13:44:35 +0300

    Test cases for Get, Delete and GetAll methods were added. Some CreateEdit() tests were refactorized

--- HotelBookingsApiFacts/HotelBookingControllerFacts.cs
+++ HotelBookingsApiFacts/HotelBookingControllerFacts.cs
@@ -12,16 +12,15 @@ namespace HotelBookingsApiFacts
         public void CreateEdit_NewBookingIsAdded_ShouldReturnExpectedResult()
         {
             var options = new DbContextOptionsBuilder<ApiContext>()
-                .UseInMemoryDatabase(databaseName: "Test_Database")
+                .UseInMemoryDatabase(databaseName: "First")
                 .Options;
 
             using var context = new ApiContext(options);
             var controller = new HotelBookingController(context);
             var newBooking = new HotelBooking { Id = 0, RoomNumber = 5, ClientName = "John" };
 
-            var result = controller.CreateEdit(newBooking);
+            controller.CreateEdit(newBooking);
 
-            Assert.NotNull(result);
             Assert.Single(context.Bookings);
             Assert.True(context.Bookings.Any(b => b.ClientName == "John"));
         }
@@ -30,21 +29,21 @@ namespace HotelBookingsApiFacts
         public void CreateEdit_BookingIsEdited_ShouldReturnExpectedResult()
         {
             var options = new DbContextOptionsBuilder<ApiContext>()
-             .UseInMemoryDatabase(databaseName: "Test_Database")
+             .UseInMemoryDatabase(databaseName: "Second")
              .Options;
 
             using var context = new ApiContext(options);
             var controller = new HotelBookingController(context);
 
             var newBooking = new HotelBooking { Id = 0, RoomNumber = 42, ClientName = "Paul" };
-            var firstBooking = controller.CreateEdit(newBooking);
+            controller.CreateEdit(newBooking);
 
             Assert.True(context.Bookings.Any(b => b.Id == 1));
             Assert.True(context.Bookings.Any(b => b.RoomNumber == 42));
             Assert.True(context.Bookings.Any(b => b.ClientName == "Paul"));
 
             var editedBooking = new HotelBooking { Id = 1, RoomNumber = 33, ClientName = "Paul" };
-            var secondBooking = controller.CreateEdit(editedBooking);
+            controller.CreateEdit(editedBooking);
 
             // same client, so id isn't changed
             Assert.True(context.Bookings.Any(b => b.Id == 1));
@@ -56,7 +55,7 @@ namespace HotelBookingsApiFacts
         public void CreateEdit_BookingIsNotFound_ShouldReturnExpectedResult()
         {
             var options = new DbContextOptionsBuilder<ApiContext>()
-             .UseInMemoryDatabase(databaseName: "Test_Database")
+             .UseInMemoryDatabase(databaseName: "Third")
              .Options;
 
             using var context = new ApiContext(options);
@@ -65,7 +64,111 @@ namespace HotelBookingsApiFacts
             var firstBooking = new HotelBooking { Id = 6, RoomNumber = 11, ClientName = "Mary" };
             var result = controller.CreateEdit(firstBooking);
 
-            Assert.IsType<NotFoundResult>(result.Value);     
+            Assert.IsType<NotFoundResult>(result.Value);
+        }
+
+        [Fact]
+        public void Get_BookingIsFound_ShouldReturnExpectedResult()
+        {
+            var options = new DbContextOptionsBuilder<ApiContext>()
+             .UseInMemoryDatabase(databaseName: "Fourth")
+             .Options;
+
+            using var context = new ApiContext(options);
+            var controller = new HotelBookingController(context);
+
+            var firstBooking = new HotelBooking { Id = 0, RoomNumber = 1, ClientName = "Mary" };
+            controller.CreateEdit(firstBooking);
+
+            Assert.IsType<OkObjectResult>(controller.Get(1).Value);
+        }
+
+        [Fact]
+        public void Get_BookingIsNotFound_ShouldReturnExpectedResult()
+        {
+            var options = new DbContextOptionsBuilder<ApiContext>()
+             .UseInMemoryDatabase(databaseName: "Fifth")
+             .Options;
+
+            using var context = new ApiContext(options);
+            var controller = new HotelBookingController(context);
+
+            var firstBooking = new HotelBooking { Id = 0, RoomNumber = 5, ClientName = "Mary" };
+            controller.CreateEdit(firstBooking);
+
+            Assert.IsType<NotFoundResult>(controller.Get(8).Value);
+        }
+
+        [Fact]
+        public void Delete_BookingIsFound_ShouldReturnExpectedResult()
+        {
+            var options = new DbContextOptionsBuilder<ApiContext>()
+            .UseInMemoryDatabase(databaseName: "Sixth")
+            .Options;
+
+            using var context = new ApiContext(options);
+            var controller = new HotelBookingController(context);
+
+            var firstBooking = new HotelBooking { Id = 0, RoomNumber = 10, ClientName = "Kimberly" };
+            controller.CreateEdit(firstBooking);
+
+            Assert.IsType<NoContentResult>(controller.Delete(1).Value);
+        }
+
+        [Fact]
+        public void Delete_BookingIsNotFound_ShouldReturnExpectedResult()
+        {
+            var options = new DbContextOptionsBuilder<ApiContext>()
+            .UseInMemoryDatabase(databaseName: "Seventh")
+            .Options;
+
+            using var context = new ApiContext(options);
+            var controller = new HotelBookingController(context);
+
+            var firstBooking = new HotelBooking { Id = 0, RoomNumber = 10, ClientName = "Kimberly" };
+            controller.CreateEdit(firstBooking);
+
+            Assert.IsType<NotFoundResult>(controller.Delete(2).Value);
+        }
+
+        [Fact]
+        public void GetAll_AllBokingsAreReturned_ShouldReturnExpectedResult()
+        {
+            var options = new DbContextOptionsBuilder<ApiContext>()
+           .UseInMemoryDatabase(databaseName: "Eighth")
+           .Options;
+
+            using var context = new ApiContext(options);
+            var controller = new HotelBookingController(context);
+
+            var firstBooking = new HotelBooking { Id = 0, RoomNumber = 4, ClientName = "Marcus" };
+            var secondBooking = new HotelBooking { Id = 0, RoomNumber = 2, ClientName = "Carlo" };
+            var thirdBooking = new HotelBooking { Id = 0, RoomNumber = 55, ClientName = "Joshua" };
+
+            controller.CreateEdit(firstBooking);
+            controller.CreateEdit(secondBooking);
+            controller.CreateEdit(thirdBooking);
+
+            Assert.Equal(3, context.Bookings.Count());
+            Assert.True(context.Bookings.Any(b => b.ClientName == "Marcus"));
+            Assert.True(context.Bookings.Any(b => b.ClientName == "Carlo"));
+            Assert.True(context.Bookings.Any(b => b.ClientName == "Joshua"));
+        }
+
+        [Fact]
+        public void GetAll_NoBookingsAvailable_ShouldReturnExpectedResult()
+        {
+            var options = new DbContextOptionsBuilder<ApiContext>()
+           .UseInMemoryDatabase(databaseName: "Nineth")
+           .Options;
+
+            using var context = new ApiContext(options);
+            var controller = new HotelBookingController(context);
+
+            var result = controller.GetAll();
+
+            Assert.Empty(context.Bookings);
+            Assert.IsType<OkObjectResult>(result.Value);
         }
     }
 }
